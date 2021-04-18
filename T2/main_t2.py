591e8bb0e7defe13ab8ae1b55b85f454e99b784d
Alessandro Melo <alessandromelooliver@gmail.com>
2021-04-18 13:53:39 -0300

    Configurações Iniciais

---
+++ T2/main_t2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 18 13:51:40 2021
+
+@author: aless
+"""
+
---
+++ T3/main_t3.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 18 13:51:58 2021
+
+@author: aless
+"""
+

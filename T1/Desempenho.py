b3dc83a8fc91062fb1275fa026e0312b349a8837
Alessandro Melo <43254253+AlessandroMDO@users.noreply.github.com>
2021-04-25 00:16:33 -0300

    Validação dados aerodinâmicos e parâmetros de desempenho
    Validação do modelo do Abner e início da implementação dos parâmetros de desempenho

--- T1/Desempenho.py
+++ T1/Desempenho.py
@@ -14,8 +14,9 @@ sys.path.append(parent_dir)
 import numpy as np
 import matplotlib.pyplot as plt
 
-from Interpolacao import CLp, Mp
+from Interpolacao import polar, param
 from aircraft import jetstar
+from Utilities.isa_atmosphere import atmo_isa as ISA
 
 
 
@@ -23,13 +24,43 @@ from aircraft import jetstar
 
 jet = jetstar(1)
 
+velo_cruzeiro = 811 / 3.6 # [m/s]
+altitude = 13105 # [m]
+densidade = ISA(altitude)[3] #[kg/m^3]
+velo_som = ISA(altitude)[2] # [m/s]
+mach = velo_cruzeiro/velo_som
+
+CL = jet.W / (0.5 * jet.S * (velo_cruzeiro**2) * densidade)
+CD = polar(param, CL, mach)
+
+E = CL/CD
+gamma = -1/E
+h_dot = velo_cruzeiro * gamma
+
+
+
+
+    
+    
+    
+
+
+
+
 beta = 9296
 sigma = lambda h: np.exp((-h/beta))
+
+
+h2 = altitude # [m]
+h1 = 0 # [m]
+
 
 
 #%% Alcance
 
 #%%%  CL constante
+
+
 
 
 

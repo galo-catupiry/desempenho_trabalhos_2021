6966b52351afa5701a710787fcf64217f5bce4f6
Alessandro Melo <43254253+AlessandroMDO@users.noreply.github.com>
2021-04-29 16:49:22 -0300

    Update código de desempenho
    Mudanças simples

--- T1/Desempenho.py
+++ T1/Desempenho.py
@@ -30,43 +30,6 @@ densidade = ISA(altitude)[3] #[kg/m^3]
 velo_som = ISA(altitude)[2] # [m/s]
 mach = velo_cruzeiro/velo_som
 
-CL = jet.W / (0.5 * jet.S * (velo_cruzeiro**2) * densidade)
-CD = polar(param, CL, mach)
-
-E = CL/CD
-gamma = -1/E
-h_dot = velo_cruzeiro * gamma
-
-
-def hdot_v_graph():
-    fig = plt.figure(figsize=(6,6))
-    
-    h_dot_list = []
-    V_list = []
-    gamma_line = []
-    
-    for i in np.linspace(1, velo_cruzeiro,500):
-        densidade = ISA(altitude)[3] #[kg/m^3]
-        velo_som = ISA(altitude)[2] # [m/s]
-        mach = i/velo_som
-
-        CL = jet.W / (0.5 * jet.S * (i**2) * densidade)
-        CD = polar(param, CL, mach)
-
-        E = CL/CD
-        gamma = -1/E
-        h_dot = i * gamma
-        
-        h_dot_list.append(h_dot)
-        V_list.append(i)
-        
-    plt.plot(V_list, h_dot_list, color = 'k')
-    plt.grid()
-    plt.xlabel("V [m/s]", fontsize = 14)
-    plt.ylabel("$\dot{h}$", fontsize = 14)
-    plt.show()
-        
-    
 beta = 9296
 sigma = lambda h: np.exp((-h/beta))
 

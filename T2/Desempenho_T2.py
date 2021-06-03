3996fe8f32ebd3c3e52cb1b0019b471be40facdd
Abnerm <abnerm@usp.br>
2021-06-03 19:44:09 -0300

    Atualização em Desempenho_T2.py
    1) Implementação da função de alcance;
    2) Inserção de comentários nas funções

--- T2/Desempenho_T2.py
+++ T2/Desempenho_T2.py
@@ -8,8 +8,7 @@ Integrantes:
     Thiago Buchignani De Amicis - 10277418
 """
 
-#%% Packages
-
+#%% Bibliotecas
 import os, sys
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
@@ -26,21 +25,32 @@ from scipy.optimize import brentq
 plt.close('all')
 
 #%% Dados Gerais
-
 jet = JetStar(1)
 
 sealevel = Atmosphere(0)
 beta = 9296
 rho0 = sealevel.density[0]
 
-#%% Functions for Cruise Flight
+#%% Funções para Voo em Cruzeiro
 
-# DragPolar object
+# Polar de arrasto
 drag_object = DragPolar()
 drag_object.CLp = 0
 
-# Total Drag
 def total_drag(V,h):
+    '''
+    
+    Parâmetros
+    ----------
+    V : Lista de velocidades definida em "MAIN".
+    h : Lista de altitudes definida em "MAIN".
+
+    Returns
+    -------
+    D_resp : Arrasto total em cruzeiro (lista)
+    Dmin_resp : Arrasto mínimo em cruzeiro (lista)
+
+    '''
     
     D_resp, Dmin_resp = [], []
     
@@ -64,8 +74,19 @@ def total_drag(V,h):
     
     return D_resp, Dmin_resp
 
-# Cruise Velocity equations
 def cruise_velocity_eq(x,h):
+    '''
+
+    Parâmetros
+    ----------
+    x : Variável do sistema
+    h : Altura analisada (lista)
+
+    Retorna
+    -------
+    equations : Lista de equações a serem resolvidas
+
+    '''
     
     CD0 = x[0]
     K = x[1]
@@ -85,9 +106,20 @@ def cruise_velocity_eq(x,h):
     
     return equations   
     
-    
-# Cruise velocity solver
 def cruise_velocity_solver(V,h,V_type):
+    '''
+    
+    Parâmetros
+    ----------
+    V : Lista de velocidades definida em "MAIN".
+    h : Lista de altitudes definida em "MAIN".
+    V_type : Define a análise para V1 ou V2.
+
+    Returns
+    -------
+    Vresp : V1 ou V2 (lista, pois variam com h)
+
+    '''
     
     Vresp = []
     D_total = total_drag(V,h)[0]
@@ -115,8 +147,19 @@ def cruise_velocity_solver(V,h,V_type):
     
     return Vresp
     
-# Buoyancy (jet)
 def jet_buoyancy(h,T0):
+    '''
+
+    Parâmetros
+    ----------
+    h : Lista de altitudes definida em "MAIN".
+    T0 : Empuxo dos quatro motores da aeronave ao nível do mar
+
+    Returns
+    -------
+    T : Lista de empuxo para cada altitude (h).
+
+    '''
     
     T = []
     
@@ -127,10 +170,35 @@ def jet_buoyancy(h,T0):
     
     return T
 
+def cruise_range(cond,V1,h,c, zeta):
+    drag_cru = DragPolar()
+    rho = Atmosphere(h).density[0]
+    
+    CL_cru = (2*jet.W)/(rho*V1**2*jet.S)
+    drag_cru.CLp = CL_cru
+    drag_cru.Mp = V1/Atmosphere(h).speed_of_sound[0]
+    CD1 = drag_cru.polar()
+    
+    E1 = CL_cru/CD1
+    Em = 4*drag_cru.K/drag_cru.CD0
+    
+    if(cond == 'h_CL'):
+        
+        x = (2*V1*E1)/c*(1-np.sqrt(1 - zeta))
+    
+    elif(cond == 'V_CL'):
+        
+        x = E1*V1/c*np.log(1/(1 - zeta))
+        
+    elif(cond == 'V_h'):
+       
+        x = (2*V1*Em)/c*np.arctan(E1*zeta/(2*Em*(1 - drag_cru.K*E1*CL_cru*zeta)))
+        
+    return x
 
 #%% Plots for Cruise Flight
 
-def TD_vs_V(V,D_total,T, Dmin):
+def TD_vs_V(h,V,D_total,T, Dmin):
     
     plt.figure(1)
     plt.xlabel("Velocity [m/s]")
@@ -159,7 +227,7 @@ def TD_vs_V(V,D_total,T, Dmin):
     plt.ylim(top = 40000)
     return
 
-def h_vs_V(V1,V2):
+def h_vs_V(h,V1,V2):
     # EM CONSTRUÇÃO!!
     
     plt.figure(2)
@@ -173,19 +241,43 @@ def h_vs_V(V1,V2):
 
 #%% MAIN
 
+# -/----------------- Propulsão ------------------/-
 n = 0.85
-h = np.arange(0,14430,100).tolist()
 T0 = 64000
+c = 0.45  # (Verificar!)
+zeta = 0.45  #  (Verificar!)
 
-V = np.linspace(0,320,200)
-[D_total,Dmin] = total_drag(V,h)
-T = jet_buoyancy(h,T0)
+# -/-------- Análise de Alcance (Cruzeiro) -------/-
+V1_cru =  811 / 3.6
+h_cru = 13105
 
-V1 = cruise_velocity_solver(V,h,'V1')
-V2 = cruise_velocity_solver(V,h,'V2')
+x1 = cruise_range('h_CL',V1_cru,h_cru,c, zeta)
+x2 = cruise_range('V_CL',V1_cru,h_cru,c, zeta)
+x3 = cruise_range('V_h',V1_cru,h_cru,c, zeta)
 
-# Figures:
+# -/--------- Parâmetros para diagramas ----------/-
+
+# Diagrama T,D vs. V
+Diagrama1 = False
+if(Diagrama1):
+    h_fig1 = [10000, 8000]
+    V_fig1 = np.linspace(70,320,200)
+    [D_total_fig1,Dmin_fig1] = total_drag(V_fig1,h_fig1)
+    T_fig1 = jet_buoyancy(h_fig1,T0)
     
-#figure_1 = TD_vs_V(V,D_total,T, Dmin)
-figure_2 =  h_vs_V(V1,V2)
+    figure_1 = TD_vs_V(h_fig1,V_fig1,D_total_fig1,T_fig1, Dmin_fig1)
+
+# Diagrama h-V
+Diagrama2 = False
+if(Diagrama2):
+    h_fig2 = np.arange(0,14430,200).tolist()
+    V_fig2 = np.linspace(0,320,200)
+    [D_total_fig2,Dmin_fig2] = total_drag(V_fig2,h_fig2)
+    T_fig2 = jet_buoyancy(h_fig2,T0)
+    
+    V1_fig2 = cruise_velocity_solver(V_fig2,h_fig2,'V1')
+    V2_fig2 = cruise_velocity_solver(V_fig2,h_fig2,'V2')
+    
+    figure_2 =  h_vs_V(h_fig2,V1_fig2,V2_fig2)
+
 

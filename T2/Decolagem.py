06198a2cab8d4adc9b95daf9ce7ead3471d3ef0f
gbpu <guilherme.beppu@usp.com>
2021-07-26 15:00:23 -0300

    Implementação do Pouso e correções da Decolagem

--- T2/Decolagem.py
+++ T2/Decolagem.py
@@ -17,7 +17,7 @@ import numpy as np
 
 from aircraft import JetStar
 from ambiance import Atmosphere
-from Interpolacao import DragPolar
+#from Interpolacao import DragPolar
 
 
 # =============================================
@@ -26,40 +26,38 @@ sealevel = Atmosphere(0)
 rho0 = sealevel.density[0]
 g = 9.81
 ft_to_m = 0.3048
-#CLmax = 2*1*jet.W/(64**2*Atmosphere(0).density[0]*jet.S)
 R_gas = 287.058 #J/(kg*K) - constante dos gases
-# =============================================
 
 # Propulsao
 n = 0.85
 T0 = 64000
 c = 0.5/3600
+# =============================================
 
 
 def air_density(Temp, h):
-    '''densidade fora da ISA; Temp em Kelvin'''
-    P=Atmosphere(h).pressure
-    Temp_isa_mod = Atmosphere(h).temperature + Temp-(273.15 + 15)
+    '''densidade fora da ISA; Temp em Celcius'''
+    P=Atmosphere(h).pressure[0]
+    Temp_isa_mod = Atmosphere(h).temperature[0] + Temp-(15)
     rho = P/(R_gas*Temp_isa_mod) 
-    return (rho/rho0)[0]
+    return rho
 
-
-def ground_acceleration(rho, V, W, CLmax, gamma_pista=0, mi=0.04):
-    L = 0.5*CLmax*jet.S*rho*V**2   
+def ground_acceleration(rho, V, W, gamma_pista=0, mi=0.04):
+    #L = 0.5*CLmax*jet.S*rho*V**2  
+    L = 1.15*W  #usando as relações (16.16) do Ojha 
     D = 0.5*jet.CD*jet.S*rho*V**2
     T = T0*((rho/rho0)**n)
     Fric = mi*(W*np.cos(gamma_pista) - L)
 
     a = (T - D - Fric - jet.W*np.sin(gamma_pista))*(g/W)
     gamma = (T-D)/W
-
     return a, gamma
-    
-def running(rho,V_S, W, CLmax, gamma_pista=0, mi=0.04):
-    global V_Lo
+
+def running(rho,V_S, W, gamma_pista=0, mi=0.04):
+    global V_Lo, gamma
     
     V_Lo = 1.2*V_S
-    a_bar = ground_acceleration(rho, 0.7*V_Lo, W, CLmax, gamma_pista=0, mi=0.04)[0]
+    a_bar = ground_acceleration(rho, 0.7*V_Lo, W, gamma_pista=0, mi=0.04)[0]
     x_G = (V_Lo**2)/(2*a_bar)
     return x_G, V_Lo
 
@@ -72,7 +70,7 @@ def rotation(V_S):
 def transition_and_climbing(rho, V_Lo, W,  CLmax):
     R_Tr = (V_Lo**2)/(0.15*g)
     h_Sc = 35*ft_to_m
-    gamma = ground_acceleration(rho, 0.7*V_Lo, W ,CLmax, 0, 0.04)[1]
+    gamma = ground_acceleration(rho, 0.7*V_Lo, W )[1]
     h_Tr = (1-np.cos(gamma))*R_Tr
     if h_Sc > h_Tr:
         x_Tr = R_Tr*np.sin(gamma)
--- T2/Pouso.py
+++ T2/Pouso.py
@@ -7,4 +7,72 @@ Integrantes:
     Guilherme Beppu de Souza    - 10696681
     Thiago Buchignani De Amicis - 10277418
 """
+# =============================================  
+import os, sys
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+import numpy as np
+
+from aircraft import JetStar
+from ambiance import Atmosphere
+#from Interpolacao import DragPolar
+
+
 # =============================================
+jet = JetStar('power approach')
+sealevel = Atmosphere(0)
+rho0 = sealevel.density[0]
+g = 9.81
+ft_to_m = 0.3048
+R_gas = 287.058 #J/(kg*K) - constante dos gases
+
+# Propulsao
+n = 0.85
+T0 = 64000
+c = 0.5/3600
+# =============================================
+
+
+#TODO: Usar Decolagem como biblioteca é melhor do q repetir a função? Muitas referencias cruzadas?
+# Ideia: Mandar as funções mais básicas (Empuxo, arrasto, air_density) para o aircraft.py
+def air_density(Temp, h):
+    '''densidade fora da ISA; Temp em Celcius'''
+    P=Atmosphere(h).pressure[0]
+    Temp_isa_mod = Atmosphere(h).temperature[0] + Temp-(15)
+    rho = P/(R_gas*Temp_isa_mod) 
+    return rho
+
+def approach_and_flare(rho, W_ap, V_S):
+    #L_f = 1.08*W_ap #Tomando aproximação (16.24-25) do Ojha
+    V_ap = 1.3*V_S
+    
+    D = 0.5*jet.CD*jet.S*rho*V_ap**2
+    T = T0*((rho/rho0)**n)
+    
+    R_f = V_ap**2/(0.08*g)   
+    h_Sc = 50*ft_to_m
+    gamma_ap = (T-D)/W_ap #gamma máximo pela potência 
+    h_f = (1-np.cos(gamma_ap))*R_f
+
+    if h_f<h_Sc:                #com altos gammas a aeronave atinge a altura sem a fase de appoach, 
+        x_f = R_f*np.sin(gamma_ap)            #limita-se o gamma por h_f=h_Sc
+        x_ap = (h_Sc - h_f)/np.tan(gamma_ap)    
+    else:
+        gamma_ap= np.arccos(-h_Sc/R_f +1)
+        x_f = R_f*np.sin(gamma_ap)
+        x_ap = 0
+    return x_f, x_ap
+
+def rotation(V_S):
+    V_R = 1.1*V_S
+    t_R = 3  
+    x_R =t_R*V_R  
+    return x_R
+
+def landing_roll(V_S):
+    V_Td = 1.1*V_S 
+    d_bar = 0.55*g
+    x_gLa = V_Td**2/(2*d_bar) 
+    return x_gLa
